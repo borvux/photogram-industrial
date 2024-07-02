dd77606d2d2436a4501667db87920c6450d758b0
borvux <bennyjoram@email.com>
2024-07-02 15:44:59 -0500

    created routes for the feed, followers, and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def feed
+    
+  end
+
+  def followers
+
+  end
+
+  def following
+
+  end
 end
