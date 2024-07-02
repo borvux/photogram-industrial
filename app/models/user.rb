0275dda402f10089e0a6450482065326cd9857f0
borvux <bennyjoram@email.com>
2024-07-01 21:29:46 -0500

    fixed errors from review

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,9 +30,10 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
+  has_many :likes, foreign_key: :fan_id
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: :author_id
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :comments, foreign_key: "author_id"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :liked_photos, through: :likes, source: :photo
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
