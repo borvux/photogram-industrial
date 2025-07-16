d85c2d854c64b3d95a8b4a6adad2305ea10d7bf8
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-07-16 20:37:36 +0000

    added photo commenting

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,12 +32,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :avatar_image, ImageUploader
+
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: :author_id
 
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
-  
+
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
