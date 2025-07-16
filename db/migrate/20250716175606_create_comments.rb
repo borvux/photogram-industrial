43d606e032242ecdbd66881f4589573a7198b42d
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-07-16 17:58:57 +0000

    Generated comments

---
+++ db/migrate/20250716175606_create_comments.rb
@@ -0,0 +1,13 @@
+# rails generate scaffold comment author:references photo:references body:text
+
+class CreateComments < ActiveRecord::Migration[8.0]
+  def change
+    create_table :comments do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users }, index: false
+      t.references :photo, null: false, foreign_key: true
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
