cbc62a788c8173ba99c6fa0bc924c37111e667a4
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-07-19 20:22:50 +0000

    added feed view following RCAV

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,8 @@ class UsersController < ApplicationController
   def discover
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
   get ":username/discover" => "users#discover", as: :discover
+  get ":username/feed" => "users#feed", as: :feed
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
