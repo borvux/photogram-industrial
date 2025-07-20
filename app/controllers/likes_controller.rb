151b7373d7667cdbb40270c98130a30a40349aa0
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-07-20 03:11:48 +0000

    added fallback_location for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -26,7 +26,7 @@ class LikesController < ApplicationController
 
     respond_to do |format|
       if @like.save
-        format.html { redirect_to @like, notice: "Like was successfully created." }
+        format.html { redirect_back fallback_location: @like.photo, notice: "Like was successfully created." }
         format.json { render :show, status: :created, location: @like }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class LikesController < ApplicationController
     @like.destroy!
 
     respond_to do |format|
-      format.html { redirect_to likes_path, status: :see_other, notice: "Like was successfully destroyed." }
+      format.html { redirect_back fallback_location: @like.photo, status: :see_other, notice: "Like was successfully destroyed." }
       format.json { head :no_content }
     end
   end
